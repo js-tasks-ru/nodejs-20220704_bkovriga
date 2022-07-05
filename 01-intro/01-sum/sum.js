f261f325f17b0b9af6a5198427108234e548286b
Bogdan G. Kovriga <48043627+labyrinth404@users.noreply.github.com>
2022-07-06 00:36:45 +0700

    fitst

--- 01-intro/01-sum/sum.js
+++ 01-intro/01-sum/sum.js
@@ -1,5 +1,10 @@
 function sum(a, b) {
   /* ваш код */
+   if(typeof a === 'number' && typeof === b 'number'){
+    return a + b;
+  }
+  console.error('TypeError');
+  return;
 }
 
 module.exports = sum;
